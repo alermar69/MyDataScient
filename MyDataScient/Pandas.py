d0726f400c4843e136ac28d7711651d53dce065f
alermar69 <alermar69@mail.ru>
2018-11-30 22:24:14 +0200

    11

--- MyDataScient/Pandas.py
+++ MyDataScient/Pandas.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 s = pd.Series([1,3,5,np.nan,6,8])
 s
 
+
 dates = pd.date_range('20130101', periods = 5)
 dates
 type(dates)
@@ -13,25 +14,25 @@ df = pd.DataFrame(np.random.randn(5,4), index = dates, columns=list('ABCD'))
 df
 np.random.randint(5,20,size=[5, 6])
 
- df2 = pd.DataFrame({'A': 1.,
-					 'B': pd.Timestamp('20130102'),
-					 'C': pd.Series(1, index=list(range(4)), dtype='float32'),
-					 'D': np.array([3]*4, dtype='int32'),
-					 'E': pd.Categorical(["test", "train", "test", "train"]),
-					 'F': 'foo'})
- df2
- pd.Timestamp('20130102')
- pd.Series(1, index=list(range(4)), dtype='float32')
- np.array([3]*4, dtype='int32')
- pd.Categorical(["test", "train", "test", "train"])
- df2.dtypes
- df2	
+df2 = pd.DataFrame({'A': 1.,
+                 'B': pd.Timestamp('20130102'),
+                 'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                 'D': np.array([3]*4, dtype='int32'),
+                 'E': pd.Categorical(["test", "train", "test", "train"]),
+                 'F': 'foo'})
+df2
+pd.Timestamp('20130102')
+pd.Series(1, index=list(range(4)), dtype='float32')
+np.array([3]*4, dtype='int32')
+pd.Categorical(["test", "train", "test", "train"])
+df2.dtypes
+df2
 
- df.index
- df.columns
- df.values
+df.index
+df.columns
+df.values
 
- df.describe()
- df.T
- df.sort_index(axis = 1, ascending = False)
- df.sort_values(by='B')
+df.describe()
+df.T
+df.sort_index(axis = 1, ascending = False)
+df.sort_values(by='B')
